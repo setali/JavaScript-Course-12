9963bbab1bc9bcacae5d6902d3bd8f9a712d7724
salim <set.ali.mousavi@gmail.com>
2022-05-24 20:50:20 +0430

    Session 2

---
+++ index.js
@@ -0,0 +1,107 @@
+// var arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+
+// var arr = [2, 'salam', { name: 'Ali' }, [2, 3]]
+
+// console.log(arr)
+
+// var arr2 = arr
+
+// arr2[0] = 5
+
+// console.log(arr)
+// console.log(arr2)
+
+// var friends = ['Ali', 'Eli', 'Qoli', 'Fati']
+
+// console.log(friends.length)
+
+// console.log(friends[0])
+// console.log(friends[1])
+// console.log(friends[3])
+// console.log(friends[4])
+
+// friends[3] = 'Zari'
+
+// console.log(friends)
+
+// friends.length = 2
+
+// console.log(friends)
+// console.log(friends[3])
+
+// AB12
+// var a = { name: 'Ali' }
+
+// // CA36
+// var b = { name: 'Ali' }
+
+// console.log(a == b)
+
+// // 23AB12
+
+// var obj1 = {
+//   name: 'Ali',
+//   lastname: 'Mousavi',
+//   age: 32,
+//   gender: true,
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar'
+//   }
+// }
+
+// console.log(obj1.name)
+
+// var obj2 = obj1
+
+// obj2.name = 'Eli'
+
+// console.log(obj1.name)
+// console.log(obj2.name)
+
+// var propertyName = 'gender'
+
+// console.log(ali.name)
+// console.log(ali[propertyName])
+// console.log(ali.propertyName)
+// console.log(ali['family'].father)
+// console.log(ali.family['mother'])
+
+// var str = "My name is \uD83D\uDE00 ali"
+
+// console.log(str)
+
+// var number
+// number = 12
+// console.log(number)
+
+// var firstName = 'Ali',
+//   lastName = 'Mousavi'
+
+// var نام = 'علی'
+
+/*
+var a = 3
+var b = a + 5
+
+// log to console
+
+console.table({ b })
+console.log(b)
+
+*/
+
+/*
+1 => 0 1
+2 => 00  01 10 11
+3 => 000 001 010 011 100 101 110 111
+4 => 15
+
+2 ** n - 1
+
+
+*/
