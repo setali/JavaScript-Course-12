6a135c6615d0097b94ef4bbcd67c9f29025469cb
SAli <set.ali.mousavi@gmail.com>
2022-06-28 20:57:50 +0430

    Session 11

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// document.body.children[0].textContent = 'Ali Mousavi'
+function setCookie (name, value, time, path = '/') {
+  var date = new Date()
+  date.setSeconds(date.getSeconds() + time)
+
+  document.cookie =
+    name + '=' + value + ';expires=' + date.toUTCString() + ';path=' + path
+}
+
+function getCookies () {
+  var cookie = document.cookie
+
+  var arr = cookie.split(';')
+
+  var result = {}
+
+  arr.forEach(function (el) {
+    var element = el.trim().split('=')
+    result[element[0]] = element[1]
+  })
+
+  return result
+}
+
+function getCookie (name) {
+  return getCookies()[name]
+}
+
+function removeCookie (name) {
+  setCookie(name, '', 0)
+}
+
+// removeCookie('name')
+// setCookie('token', 'asdafsrgerweffaasfvs', 20 * 60)
